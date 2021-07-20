723ec1a9a686662605af32399d3caecc601e34d0
C-Kristel <85912319+C-Kristel@users.noreply.github.com>
2021-07-20 11:20:15 +0800

    edit file name

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const Contacts = require('../models/Contacts');
-const verify = require('./verifyToken');
+const verify = require('./token');
 const {
     contactValidation
 } = require('../validation');
